9712336a02175ed006c02b0f6fd2a67f1fe5ef77
mihon.testosterone <mihon.testosterone@gmail.com>
2021-09-24 21:13:16 +0300

    redux started

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,24 +1,34 @@
 import types from "./types";
 import { v4 as uuidv4 } from "uuid";
+import { createAction } from "@reduxjs/toolkit";
 
-const addContact = ({ name, number }) => ({
-  type: types.ADD,
+const addContact = createAction(types.ADD, ({ name, number }) => ({
   payload: {
     id: uuidv4(),
     name,
     number,
   },
-});
+}));
+// const addContact = ({ name, number }) => ({
+//   type: types.ADD,
+//   payload: {
+//     id: uuidv4(),
+//     name,
+//     number,
+//   },
+// });
 
-const deleteContact = (id) => ({
-  type: types.DELETE,
-  payload: id,
-});
+const deleteContact = createAction(types.DELETE);
+// const deleteContact = (id) => ({
+//   type: types.DELETE,
+//   payload: id,
+// });
 
-const changeFilter = (value) => ({
-  type: types.CHANGE_FILTER,
-  payload: value,
-});
+const changeFilter = createAction(types.CHANGE_FILTER);
+// const changeFilter = (value) => ({
+//   type: types.CHANGE_FILTER,
+//   payload: value,
+// });
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { addContact, deleteContact, changeFilter };
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer'
+import logger from 'redux-logger'
+
+// const middleware = [...getDefaultMiddleware(), logger]
 
 const store = configureStore({
     reducer: {
         contacts: reducer
     },
+    // middleware,
     devTools: process.env.NODE_ENV === 'development',
 })
 export default store
